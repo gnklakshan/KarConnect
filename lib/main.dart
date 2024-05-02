6b71ec8f035a834446a1a5206014130c1e2845bb
gnklakshan <132909147+gnklakshan@users.noreply.github.com>
2024-05-02 16:13:01 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:karconnect/app.dart';
 import 'package:karconnect/backend/firebase/firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
